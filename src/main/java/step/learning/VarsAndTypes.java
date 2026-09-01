2f76b4b03dbe18d978c1b99dafbe5c1656034912
illia <ilyagliba02@gmail.com>
2026-09-01 12:04:10 +0300

    1st commit

---
+++ src/main/java/step/learning/VarsAndTypes.java
@@ -0,0 +1,23 @@
+package step.learning;
+
+public class VarsAndTypes {
+    public void run() {
+        System.out.println("vars and types");
+
+        boolean b = true;
+        byte bb = 120;
+        short s = 12000;
+        int i = 12000000;
+        long l = 12000000L;
+
+        System.out.println(s + "");
+        float f = 100;
+        double d = 1.1e-7;
+        f = 1e3f;
+        Float f2 = f;
+        f2.toString();
+        char c = 'Ї';
+        Character cr = c;
+        cr.toString();
+    }
+}
